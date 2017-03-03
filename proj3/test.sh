19bf92e4a20c50c1676585a145026175268ad758
kelvinabrokwa <kelvinabrokwa@gmail.com>
2017-03-02 21:37:34 -0500

    test adjustments

--- proj3/test.sh
+++ proj3/test.sh
@@ -11,7 +11,47 @@ separator() {
 
 printf "Testing\n"
 
+export UNITS=
+export TMOM=
+export TSTALL=
+
 separator
+ls | ./totalsize
+separator
+(export UNITS=k && ls | ./totalsize)
+separator
+(export UNITS=K && ls | ./totalsize)
+separator
+(export UNITS=bogus && ls | ./totalsize)
+separator
+(export TMOM=-1 && ls | ./totalsize)
+separator
+(export TMOM=10000000 && ls | ./totalsize)
+separator
+(export TMOM=0 && ls | ./totalsize)
+separator
+(export TSTALL=0 && ls | ./totalsize)
+separator
+(export TSTALL=-1 && ls | ./totalsize)
+separator
+(export TSTALL=1 && ls | ./totalsize)
+separator
+(export UNITS=k && export TMOM=-1 && export TSTALL=1 && ls | ./totalsize)
+separator
+
+ls | ./accessed 1
+separator
+ls | ./accessed -1
+separator
+ls | ./accessed 100000000000
+separator
+ls | ./accessed -100000000000
+separator
+ls | ./accessed bogus
+separator
+ls | ./accessed -bogus
+separator
+
 ls | ./report 1
 separator
 ls | ./report 1 -d 1
@@ -37,15 +77,19 @@ separator
 ls | ./report 1 -k -d
 separator
 
+(cd pgm3test.d/ && ./script)
 find ./pgm3test.d/subdir/subsubdir -print | ./totalsize
 separator
 
+(cd pgm3test.d/ && ./script)
 find ./pgm3test.d -print | ./totalsize
 separator
 
+(cd pgm3test.d/ && ./script)
 find ./pgm3test.d/subdir/subsubdir -print | ./accessed 5
 separator
 
+(cd pgm3test.d/ && ./script)
 find ./pgm3test.d -print | ./report 5
 separator
 
